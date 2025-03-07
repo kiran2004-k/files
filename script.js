804d6bfd6c4f23993f8fbae01228dd9a6eeb1432
kiran2004-k <kiranyadav1572004@gmail.com>
2025-03-07 20:25:00 +0530

    all the files are commited

--- script.js
+++ script.js
@@ -9,10 +9,25 @@ function favourite ()
     console.log("favourite singer:"+fsinger)
 
 }
-favourite()*/
+favourite()
 
 function area(length,breath)
 {
     console.log("area is:"+length*breath)
 }
+
 area(5,2)
+function add(a,b)
+{
+    return a+b
+}
+ var a= add(10,20)
+ console.log(a)*/
+ if(false)
+ {
+    console.log("it is true")
+
+ }
+ else{
+    console.log("its not true")
+ }
