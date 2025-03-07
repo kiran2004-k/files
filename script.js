846e39589c311ffa0396513a32e226cc1d3ee5e6
kiran2004-k <kiranyadav1572004@gmail.com>
2025-03-07 19:38:42 +0530

    git and index is deleted

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+/*var factor="vijay"
+var fplayer="dhoni"
+var fsinger="ani"
 
+function favourite ()
+{
+    console.log("favourite actor:"+factor)
+    console.log("favourite player:"+fplayer)
+    console.log("favourite singer:"+fsinger)
+
+}
+favourite()*/
+
+function area(length,breath)
+{
+    console.log("area is:"+length*breath)
+}
+area(5,2)
