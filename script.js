755260cd6f60d342bfd09435c797f88ad9514089
kiran2004-k <kiranyadav1572004@gmail.com>
2025-03-08 22:43:42 +0530

    all files are commited

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function add(a,b)
     return a+b
 }
  var a= add(10,20)
- console.log(a)*/
+ console.log(a)
  if(false)
  {
     console.log("it is true")
@@ -31,4 +31,67 @@ function add(a,b)
  else{
     console.log("its not true")
  }
- 
+var homework= false
+ if(homework)
+ {
+    console.log("great job")
+
+ }
+ else{
+    console.log("finish your home work before playing")
+ }
+var season = "dd"
+if(season=="summer")
+{
+    console.log("fun with the sun")
+}
+if(season=="winter")
+    {
+        console.log("go with the snow")
+    }
+    if(season=="autumn")
+        {
+            console.log("fast the past")
+        }
+        
+            if(season=="spring")
+                {
+                    console.log("sing with spring")
+                }
+                else {
+                    console.log("no seasons")
+}
+var score=50
+if(score<=50)
+{
+    console.log("ok better try next time")
+}
+function checkoddeven(number)
+{
+    if(number % 2==0){
+        console.log(number+"is even")
+    }
+    else{
+        console.log(number  +"is odd")
+    }
+}
+checkoddeven(11)
+for(count=1;count<=10;count=count+2)
+{
+    console.log(count)
+
+}
+
+    for(count=1;count<=10;count=count*2
+    )
+    {
+        if(count % 2===0)
+        {
+            console.log(count)
+        }
+    }*/
+   for(count=1;count<=10;count=count+1)
+   {
+    console.log(count +"X2=" + count*2)
+   }
+
